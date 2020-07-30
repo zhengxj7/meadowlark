e89fa90bcf6b891537302d7382eaf387cde39dc9
zhengxianjun <1450734240@qq.com>
2020-07-30 19:36:32 +0800

    moved 'fortune cookie' functionality into module.

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main'
 });
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -25,8 +19,7 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
     // res.type('text/plain');
     // res.send('About Meadowlark Travel');
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all 处理器（中间件）
